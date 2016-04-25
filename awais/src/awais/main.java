de24aad749fbf1f966e923b733763c74ad960fb6
ranaawais142 <ranaawaisse142@gmail.com>
2016-04-25 17:40:25 +0500

    your firts commits//////////////////////////////////////

---
+++ awais/src/awais/main.java
@@ -0,0 +1,14 @@
+package awais;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hy abcd");
+		
+		System.out.println("hy change");
+		System.out.println("hy to all");
+		
+
+	}
+
+}
